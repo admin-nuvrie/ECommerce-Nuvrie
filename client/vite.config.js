96b6dcc4fa64808f89b29d51c59a60a06f156fab
admin-nuvrie <admin@nuvrie.com>
2025-07-27 18:50:01 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/client/', // 👈 Add this line for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
